5d8de2ed27ece7ca9f1132244aca3de60f3b900d
XoticRealmsOwnerandDev <XoticRealmshelp@gmail.com>
2018-03-27 20:31:28 -0500

    Update KitMainMenu.php

--- src/Infernus101/KitUI/UI/windows/KitMainMenu.php
+++ src/Infernus101/KitUI/UI/windows/KitMainMenu.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 namespace Infernus101\KitUI\UI\windows;
 
 use Infernus101\KitUI\Main;
@@ -9,7 +9,7 @@ use pocketmine\utils\TextFormat;
 use pocketmine\network\mcpe\protocol\ModalFormRequestPacket;
 use pocketmine\network\mcpe\protocol\ModalFormResponsePacket;
 use pocketmine\Player;
-
+ 
 class KitMainMenu extends Window {
 	public function process(): void {
 		$url = "";
@@ -24,19 +24,20 @@ class KitMainMenu extends Window {
 		];
 		foreach($this->pl->kits as $name => $data){
 			$name = ucfirst($name);
+			$name2 = $name;
 			$kits = $this->pl->getKit($name);
 			if(isset($kits->data["kit-name"]))	
-				$name = $kits->data["kit-name"];
+				$name2 = $kits->data["kit-name"];
 			if(isset($kits->data["image-url"])){
 			$url = $kits->data["image-url"];
-			$this->data["buttons"][] = ["text" => "$name", "image" => ["type" => "url", "data" => $url]];
+			$this->data["buttons"][] = ["text" => "$name2", "image" => ["type" => "url", "data" => $url]];
 			}else{
-			$this->data["buttons"][] = ["text" => "$name"];	
+			$this->data["buttons"][] = ["text" => "$name2"];	
 			}
 			array_push(parent::$id, "$name");
 		}
 	}
-
+ 
 	public function handle(ModalFormResponsePacket $packet): bool {
 		$index = (int) $packet->formData;
 		$windowHandler = new Handler();
